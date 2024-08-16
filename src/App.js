aa8c2d611dea8c3bccc7fe2c71a4c5098cc7482f
Bulent Buyukkahraman <bulent@buyukkahraman.com>
2024-08-16 17:44:13 +0300

    Update App.js

--- src/App.js
+++ src/App.js
@@ -10,7 +10,7 @@ function App() {
 
         <section id="about">
           <h3>About</h3>
-          <p>Bülent Büyükkahraman graduated from Istanbul Technical University's Department of Electronics and Communication Engineering in 1996. Throughout his career, he has successfully held various cybersecurity roles ranging from engineering to directorship at international companies such as IBM, Nokia, Coca-Cola, and leading Turkish companies such as Anadolu Group, Turkcell, and Netaş. He established the Axoss Cybersecurity Academy, contributing to the development of the workforce in the cybersecurity industry. Since 2022, he has been continuing his work as the Cybersecurity Manager of Anadolu Group.</p>
+          <p>Bülent Büyükkahraman graduated from Istanbul Technical University's Department of Electronics and Communication Engineering in 1996. Throughout his career, he has successfully held various cybersecurity roles ranging from engineering to directorship at international companies such as IBM, Nokia, Coca-Cola, and leading Turkish companies such as Anadolu Group, Turkcell, and Netaş. He established the Axoss Cybersecurity Academy, contributing to the development of the workforce in the cybersecurity industry. Since 2022, he has been continuing his work as the Cybersecurity Director of Anadolu Group.</p>
         </section>
 
         <section id="experience">
